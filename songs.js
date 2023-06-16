1432154101dd660d876c1ecdc364b5d8082539e0
Isac <isac.mamede14@gmail.com>
2023-06-15 21:02:23 -0300

    project started

---
+++ songs.js
@@ -0,0 +1,18 @@
+export default[
+    {
+        src: "./src/Gangsta's Paradise.mp3",
+        name: "Gangsta's Paradise"
+    },
+    {
+        src: "./src/In The End - Linkin Park .mp3",
+        name: "In The End - Linkin Park"
+    },
+    {
+        src: "./src/Mad Mad World (feat. Sizzla Kalonji & Collie Buddz).mp3",
+        name: "Mad Mad World"
+    },
+    {
+        src: "./src/src/Smack That - Akon.mp3",
+        name: "Smack That - Akon"
+    }
+]
